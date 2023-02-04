269600f29a217f21dc0b366eb6fdcef4c5b8409c
sanfrancis <sanfrancis006@gmail.com>
2023-02-04 12:55:37 +0530

    initial commit

---
+++ src/test/java/com/webapp/concretedetailer/ConcretedetailerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.concretedetailer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcretedetailerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
